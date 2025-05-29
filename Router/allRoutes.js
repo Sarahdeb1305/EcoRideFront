1ea17c3fc0251250ca8b2edf73b24b47299ce075
Sarah de Bozzi <debozzisarah@hotmail.com>
2025-05-29 17:01:22 +0200

    Finish Covoiturages page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,5 +1,8 @@
 import Route from "./Route.js";
 //Définir ici vos routes
-export const allRoutes = [new Route("/", "Accueil", "/pages/home.html")];
+export const allRoutes = [
+  new Route("/", "Accueil", "/pages/home.html"),
+  new Route("/covoiturages", "Accès covoiturages", "/pages/covoiturages.html"),
+];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EcoRide";
